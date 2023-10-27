3f4f56a9781e3cc0fc5673c65a4ecafb46465e22
Rupam <rupam.code@gmail.com>
2023-10-27 17:24:59 +0530

    v-2.0

--- hexa_deci_octalFormated.c
+++ hexa_deci_octalFormated.c
@@ -2,12 +2,18 @@
 
 void main ()
 {
+    //variable
     int num;
+    //@printf for showing what type of input
     printf("Enter a Decimal Number\n:");
+    //input from user
     scanf("%i",&num);
 
+    //print the decimal value
     printf("Decimal Number: %i\n",num);
+    //print the octal value
     printf("Octal Number: %o\n",num);
+    //print the hexadecimal value
     printf("Hexadecimal Number: %X\n",num);
 
 
