4091005b1527c22fb57a1f70eafac6ee607a44a3
Rupam <rupam.roy09876@gmail.com>
2023-10-27 17:05:21 +0530

    V-1.0

---
+++ hexa_deci_octalFormated.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main ()
+{
+    int num;
+    printf("Enter a Decimal Number\n:");
+    scanf("%i",&num);
+
+    printf("Decimal Number: %i\n",num);
+    printf("Octal Number: %o\n",num);
+    printf("Hexadecimal Number: %X\n",num);
+
+
+}
